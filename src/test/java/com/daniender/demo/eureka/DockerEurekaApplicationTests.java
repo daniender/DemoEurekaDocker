55870251e11d56a9a48d2f4fd28c280130ca322d
= <daniender@gmail.com>
2021-03-30 13:54:16 +0200

    Initial commit

---
+++ src/test/java/com/daniender/demo/eureka/DockerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniender.demo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
